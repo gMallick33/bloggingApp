a5d6bc3aa92ddfd4b7c80085e5059caf82a8b080
gyani <g.arya.mallick@gmail.com>
2023-05-12 12:39:56 +0530

    initialized the project, added the dependencies

---
+++ bloggingApp/src/main/java/com/bloggingApp/bloggingApp/BloggingAppApplication.java
@@ -0,0 +1,13 @@
+package com.bloggingApp.bloggingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingAppApplication.class, args);
+	}
+
+}
