bcba365076e4c1168a556ba08d51a22bd0bbfd89
gyani <g.arya.mallick@gmail.com>
2023-05-14 22:35:04 +0530

    added apis for User in UserController.java and added ApiResponse.java

--- bloggingApp/src/main/java/com/bloggingApp/bloggingApp/controllers/UserController.java
+++ bloggingApp/src/main/java/com/bloggingApp/bloggingApp/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.bloggingApp.bloggingApp.controllers;
 
+import com.bloggingApp.bloggingApp.payloads.ApiResponse;
 import com.bloggingApp.bloggingApp.payloads.UserDto;
 import com.bloggingApp.bloggingApp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
@@ -18,5 +22,25 @@ public class UserController {
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
         UserDto createdUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
+    }
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uId){
+        UserDto updatedUser = this.userService.updateUser(userDto, uId);
+        return ResponseEntity.ok(updatedUser);
+    }
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable("userId") Integer uid){
+        this.userService.deleteUser(uid);
+        return new ResponseEntity(new ApiResponse("User deleted successfully", true), HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        return ResponseEntity.ok(this.userService.getAllUsers());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId){
+        return ResponseEntity.ok(this.userService.getUserById(userId));
     }
 }
---
+++ bloggingApp/src/main/java/com/bloggingApp/bloggingApp/payloads/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.bloggingApp.bloggingApp.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private boolean successful;
+
+}
